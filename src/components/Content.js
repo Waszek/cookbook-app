0d6e1cbdb48136bda1345f60470e79784894be81
Waszek <omen20@op.pl>
2020-03-16 19:52:22 +0100

    add silder and style to header

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import image1 from '../images/image1.jpg';
 import '../styles/Content.css';
 
 
 function Content(){
     return(
-        <p>działa</p>
+        <>
+            <p>działa</p>
+        </>
     )
 }
 
