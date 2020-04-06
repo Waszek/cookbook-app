81e0d7d8cb5ace25b6183e45d0805b5fb962eb27
Waszek <omen20@op.pl>
2020-04-06 14:23:55 +0200

    Updates

---
+++ precache-manifest.309dbd3729e770b1e27394ec41c98711.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8749f1ad547bb92d8e5e7ceba751f38",
+    "url": "/cookbook-app/index.html"
+  },
+  {
+    "revision": "af704cde478b68eff676",
+    "url": "/cookbook-app/static/css/main.efabb6b9.chunk.css"
+  },
+  {
+    "revision": "c214df1cfa206d2e880d",
+    "url": "/cookbook-app/static/js/2.c43e385a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/cookbook-app/static/js/2.c43e385a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af704cde478b68eff676",
+    "url": "/cookbook-app/static/js/main.d30d0d35.chunk.js"
+  },
+  {
+    "revision": "431ce801042125a1c01c",
+    "url": "/cookbook-app/static/js/runtime-main.71543dd7.js"
+  },
+  {
+    "revision": "833c4a62eb6ce6c45d206aa47e2f7392",
+    "url": "/cookbook-app/static/media/break_img1.833c4a62.jpg"
+  },
+  {
+    "revision": "d2917f38ea65c7d3f845fb0c8ffeaa3f",
+    "url": "/cookbook-app/static/media/break_img2.d2917f38.jpg"
+  },
+  {
+    "revision": "518fb3a3cec484782e6b7f195ea25bdf",
+    "url": "/cookbook-app/static/media/break_img3.518fb3a3.jpg"
+  },
+  {
+    "revision": "4197c5104a6bc612c56ec9dbeb6943e7",
+    "url": "/cookbook-app/static/media/break_img4.4197c510.jpg"
+  },
+  {
+    "revision": "f74e63ee7e212744775ed294d10337f8",
+    "url": "/cookbook-app/static/media/cocktail_img1.f74e63ee.jpg"
+  },
+  {
+    "revision": "d6e550df8b42a74d0d336a4145b8d702",
+    "url": "/cookbook-app/static/media/cocktail_img2.d6e550df.jpg"
+  },
+  {
+    "revision": "74c550f5a0e75893e175d6b73159dd2e",
+    "url": "/cookbook-app/static/media/cocktail_img3.74c550f5.jpg"
+  },
+  {
+    "revision": "22d8e808ba994c01ca3fae5c64960b9a",
+    "url": "/cookbook-app/static/media/dessert_img1.22d8e808.jpg"
+  },
+  {
+    "revision": "e6e6db4548f807e46481bf17521ac610",
+    "url": "/cookbook-app/static/media/dessert_img2.e6e6db45.jpg"
+  },
+  {
+    "revision": "6e144ed70f26414b1005848959574e06",
+    "url": "/cookbook-app/static/media/dessert_img3.6e144ed7.jpg"
+  },
+  {
+    "revision": "817bce729abf45b01169ee30940a1465",
+    "url": "/cookbook-app/static/media/dinner_img1.817bce72.jpg"
+  },
+  {
+    "revision": "14e0a904cf287e909f810f2809202e51",
+    "url": "/cookbook-app/static/media/dinner_img2.14e0a904.jpg"
+  },
+  {
+    "revision": "5c3ea78169b8d172a5474824dbda2465",
+    "url": "/cookbook-app/static/media/dinner_img3.5c3ea781.jpg"
+  },
+  {
+    "revision": "01687d6d904096b71b198be3ecf5511d",
+    "url": "/cookbook-app/static/media/image1.01687d6d.jpg"
+  },
+  {
+    "revision": "879ac801763304a550b31c11b76b42f1",
+    "url": "/cookbook-app/static/media/image2.879ac801.jpg"
+  },
+  {
+    "revision": "13259e3dd18316094ad6e7d9297424f0",
+    "url": "/cookbook-app/static/media/image3.13259e3d.jpg"
+  },
+  {
+    "revision": "744acd250260d7e14b4bee8afed32f25",
+    "url": "/cookbook-app/static/media/soup_img1.744acd25.jpg"
+  },
+  {
+    "revision": "fa78a255c3d708464b2d7a534545f10d",
+    "url": "/cookbook-app/static/media/soup_img2.fa78a255.jpg"
+  },
+  {
+    "revision": "a5eda34d4baa5538f0b939bef586aaf6",
+    "url": "/cookbook-app/static/media/soup_img3.a5eda34d.jpg"
+  },
+  {
+    "revision": "fbb9d5b5782b609e0c8cc40e1f90b850",
+    "url": "/cookbook-app/static/media/supper_img1.fbb9d5b5.jpg"
+  },
+  {
+    "revision": "cdd94a1e2a036494fc6a422cd6184b8f",
+    "url": "/cookbook-app/static/media/supper_img2.cdd94a1e.jpg"
+  },
+  {
+    "revision": "81eee871320db6c8472f9bd04beaa618",
+    "url": "/cookbook-app/static/media/supper_img3.81eee871.jpg"
+  },
+  {
+    "revision": "9e277a31b2ca019f209a4a816b20167c",
+    "url": "/cookbook-app/static/media/vege_img1.9e277a31.jpg"
+  },
+  {
+    "revision": "5cfb58635cc9f250e06771405ecf6da2",
+    "url": "/cookbook-app/static/media/vege_img2.5cfb5863.jpg"
+  },
+  {
+    "revision": "96cb75e1fbbede849e2f314d1433ba42",
+    "url": "/cookbook-app/static/media/vege_img3.96cb75e1.jpg"
+  }
+]);
